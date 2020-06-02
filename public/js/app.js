06ebb9595df66de78ae194d12c5cf91b0690df06
Ashutosh Harkare <ashutoshharkare74@gmail.com>
2020-06-02 14:50:00 +0530

    Unnecessary console.log removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript.')
-
 const Weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
